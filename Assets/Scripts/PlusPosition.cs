86ba8e9ca9b778519707214c6e92b2d87811f963
Galym Tlektesov <galym2000gt@gmail.com>
2020-02-28 11:32:27 +0600

    Убрана надпись Vuforia engine, за счет приближения background

---
+++ Assets/Scripts/PlusPosition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlusPosition : MonoBehaviour
+{
+    public MeshRenderer backGround;
+    public float plus = 125.0f;
+    public float zPos;
+
+    void FixedUpdate()
+    {
+        if (Time.time < 0.75f)
+        {
+            backGround = GetComponentInChildren<MeshRenderer>();
+            zPos = backGround.transform.position.z;
+        }
+        if (Time.time > 0.75f)
+        {
+            backGround.transform.position = new Vector3(backGround.transform.position.x,
+                backGround.transform.position.y, zPos - plus);
+        }
+    }
+}
--- Assets/Scripts/ScalePlus.cs
+++ Assets/Scripts/ScalePlus.cs
@@ -2,28 +2,25 @@
 
 public class ScalePlus : MonoBehaviour
 {
-    public GameObject backGround;
+    public MeshRenderer backGround;
     public float plus = 125.0f;
     public float xScale, zScale;
-    public bool isPlus = false;
 
-    void Update()
-    {       
-        backGround = GameObject.Find("BackgroundPlane");
-        backGround.transform.localScale = new Vector3(1123.644f, backGround.transform.localScale.y, 873.9829f);
-        //if (backGround != null)
-        //{
-        //    xScale = backGround.transform.localScale.x;
-        //    zScale = backGround.transform.localScale.z;
-        //}
+    private void Start()
+    {
+        backGround = GetComponentInChildren<MeshRenderer>();
     }
 
-    //private void FixedUpdate()
-    //{
-    //    if (isPlus == false && xScale == backGround.transform.localScale.x)
-    //    {
-    //        backGround.transform.localScale = new Vector3(xScale + plus, backGround.transform.localScale.y, zScale + plus);
-    //        isPlus = true;
-    //    }
-    //}
+    void FixedUpdate()
+    {
+        if (Time.time < 0.75f)
+        {
+            xScale = backGround.transform.localScale.x;
+            zScale = backGround.transform.localScale.z;
+        }
+        if (Time.time > 0.75f)
+        {
+            backGround.transform.localScale = new Vector3(xScale + plus, 1.0f, zScale + plus);
+        }
+    }
 }
