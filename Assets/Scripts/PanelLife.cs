246d90997c201b26eb12a70ed66be6194be833a7
Galym Tlektesov <galym2000gt@gmail.com>
2020-03-02 17:53:18 +0600

    Изменён таргет, на тот что должен был быть там изначально

---
+++ Assets/Scripts/PanelLife.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PanelLife : MonoBehaviour
+{
+    public float timeLife = 0.75f;
+
+    void FixedUpdate()
+    {
+        if(Time.time > timeLife)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
